51e0af219efd9ce99d13b9737d1e5a7e361a713a
terygasmen <terygasmen@gmail.com>
2022-05-20 18:28:49 +0800

    added js script

---
+++ lesson 5/js/script.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+button.addEventListener('click', () => {
+    const myItem = input.value;
+    input.value = " ";
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement("span");
+    const listButton = document.createElement("button");
+
+    listItem.appendChild(listText);
+    listText.textContent = myItem;
+    listItem.appendChild = "Delete";
+    list.appendChild(listItem);
+
+    listButton.addEventListener('click', () => {
+        list.removeChild(listItem);
+    });
+
+    input.focus();
+});
