f635e824f54c147f0a159528c110a508c6c9ea9c
terygasmen <terygasmen@gmail.com>
2022-06-01 18:46:43 +0800

    added footer

---
+++ lesson3/js/scripts.js
@@ -0,0 +1,34 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + ", " + d.getDate() + " " + monthName + " " + d.getFullYear();
+
+document.getElementById("date_today").textContent = fulldate;
+document.getElementById("year_today").textContent = year;
+document.getElementById("timestamp_today").textContent = d;
