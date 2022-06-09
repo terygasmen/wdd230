fbffa0d53a1b04b60bfc120929ce49d1344c5240
terygasmen <terygasmen@gmail.com>
2022-06-09 18:17:27 +0800

    fixed chamber home page

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -23,12 +23,25 @@ let months = [
     "December",
 ];
 
+const meetGreetMsg = document.getElementById("home-meet-greet-msg");
 let d = new Date();
 let dayName = daynames[d.getDay()];
 let monthName = months[d.getMonth()];
 let year = d.getFullYear();
 let fulldate = dayName + ", " + d.getDate() + " " + monthName + " " + d.getFullYear();
 
+if(dayName === 1 || dayName === 2) {
+    meetGreetMsg.style.display = "block";
+}
+else {
+    meetGreetMsg.style.display = "none";
+}
+
 document.getElementById("date_today").textContent = fulldate;
 document.getElementById("year_today").textContent = year;
 document.getElementById("timestamp_today").textContent = d;
+
+
+
+
+
