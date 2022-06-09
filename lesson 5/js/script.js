915e3e8183013ae4c816fae04451589d03bb4b93
terygasmen <terygasmen@gmail.com>
2022-06-09 17:32:45 +0800

    fixed bom dom

--- lesson 5/js/script.js
+++ lesson 5/js/script.js
@@ -1,3 +1,5 @@
+
+/*
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
@@ -21,4 +23,33 @@ button.addEventListener('click', () => {
 
     input.focus();
 
+}); */
+
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+button.addEventListener('click', function() {
+    if(input.value != '') {
+        const inputValue = input.value;
+        input.value = '';
+
+        let listItem = document.createElement('li');
+        let deleteButton = document.createElement('button');
+
+        deleteButton.setAttribute('aria-label', `Remove ${inputValue}`);
+
+        listItem.innerHTML = inputValue;
+        deleteButton.textContent = '❌';
+
+        listItem.appendChild(deleteButton);
+        list.appendChild(listItem);
+
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(listItem);
+            input.focus();
+        });
+
+        input.focus();
+    }
 });
