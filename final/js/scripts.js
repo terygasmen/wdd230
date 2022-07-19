5f0276d876c7111882380cc868b4aa5db7bb4c18
terygasmen <terygasmen@gmail.com>
2022-07-20 02:47:05 +0800

    added date modified in footer

--- final/js/scripts.js
+++ final/js/scripts.js
@@ -6,3 +6,19 @@ function toggleMenu() {
 const x = document.getElementById("burger_icon");
 
 x.onclick = toggleMenu;
+
+
+const now = new Date();
+const fullDate = new Intl.DateTimeFormat("en-US", {
+	dateStyle: "full"
+}).format(now);
+const year = now.getFullYear();
+const today = now.getDay();
+
+document.querySelector("#copyright_year").innerHTML = year;
+
+document.getElementById("copyright_update").innerHTML = document.lastModified;
+
+document.querySelector("#copyright_year_2").innerHTML = year;
+
+document.getElementById("copyright_update_2").innerHTML = document.lastModified;
